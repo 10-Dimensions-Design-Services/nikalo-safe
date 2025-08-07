cb438d0e9db7911cda2e85450e5c79d4c79f726a
Sri Prasannanjaneyulu <anandkalvakolanu@gmail.com>
2025-08-07 16:37:39 +0530

    Merged the feature/Solution-Page in to the development branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-<<<<<<< HEAD
 import SolutionPage from './pages/SolutionPage';
-=======
 import AboutUs from './pages/AboutUs';
->>>>>>> c4a612bc18f576ce45ab2c00050b2153a00ad3fa
 
 function App() {
   return (
@@ -13,11 +10,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
           <Route path="/solution" element={<SolutionPage />} />
-=======
           <Route path="/about" element={<AboutUs />} />
->>>>>>> c4a612bc18f576ce45ab2c00050b2153a00ad3fa
         </Routes>
       </div>
     </Router>
